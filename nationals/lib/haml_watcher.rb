cfa9a8324b9edecebd830ce11bf556f2dfb517ad
Alex Bird <alexebird@gmail.com>
2010-02-22 21:00:38 -0600

    Got Haml Watcher working.  Had to use latest fssm gem source from github.

--- nationals/lib/haml_watcher.rb
+++ nationals/lib/haml_watcher.rb
@@ -1,10 +1,9 @@
 require 'rubygems'
 require 'haml'
 require 'haml/engine'
-require 'fssm'
-require 'fssm/backends/polling'
+require 'fssm'  # Using the fssm from project's lib dir.
 
-class HamlMonitor
+class HamlWatcher
   def initialize
     @haml_src = 'haml'
     @html_target = 'public'
@@ -16,7 +15,7 @@ class HamlMonitor
 
   def run
     haml_mon = self
-    FSSM.monitor("/home/bird/cs/uwcycling/nationals/#@haml_src", '**/*.haml') do
+    FSSM.monitor(@haml_src, '**/*.haml') do
       update {|b, r| haml_mon.build(r) }
       delete {|b, r| haml_mon.rm(r) }
       create {|b, r| haml_mon.build(r) }
@@ -27,15 +26,16 @@ class HamlMonitor
     html_fname = haml_to_html_fname(haml_fname)
     begin
       FileUtils.rm(html_fname)
-      Dir.rmdir(File.dirname(html_fname))
+      dirn = File.dirname(html_fname)
+      Dir.rmdir(dirn) if Dir[dirn].entries.size == 2
       puts 'rm ' + html_fname
-    rescue StandardError
-      puts 'error'
+    rescue StandardError => e
+      puts 'rm: ' + e.message
     end
   end
 
   def build(haml_fname)
-    return unless File.exists? haml_fname
+    #return unless File.exists? haml_fname
     html_fname = haml_to_html_fname(haml_fname)
     puts 'build ' + haml_fname + ' -> ' + html_fname
     dirname = File.dirname(html_fname)
@@ -45,6 +45,3 @@ class HamlMonitor
     end
   end
 end
-
-monitor = HamlMonitor.new
-monitor.run
