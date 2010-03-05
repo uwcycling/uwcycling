68289d8019bfd5db3f59873b3bd4ecfd5cc0b63c
UW Cycling <accounts@uwcycling.com>
2010-03-04 23:58:49 -0600

    App Model and App Controller initial commits

---
+++ volunteers/app/app_controller.php
@@ -0,0 +1,19 @@
+<?php
+class AppController extends Controller {
+	var $components = array('Auth');
+	
+	function beforeFilter() {
+		Security::setHash('sha1');
+		
+		$this->Auth->userModel = 'Volunteers';
+		
+		$this->Auth->fields = array(
+			'username'=>'email',
+			'password'=>'password'
+		);
+		
+		// Allow everything...for now
+		$this->Auth->allow('*');
+	}
+}
+?>
